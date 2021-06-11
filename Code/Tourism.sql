143f2b1e7e636dc8f283e04de2873167d1c7b1dd
Ioannis Loudaros <johnloudaros@hotmail.com>
2021-06-11 05:24:04 +0300

    Forgot to update

--- Code/Tourism.sql
+++ Code/Tourism.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `Nights_spent` (
   `country` varchar(255),
   `residency` varchar(255),
-  `number` float,
-  `year` varchar(255)
+  `number` bigint,
+  `year` int
 );
 
 CREATE TABLE `Arrivals` (
   `country` varchar(255),
   `residency` varchar(255),
-  `number` float,
-  `year` varchar(255)
+  `number` bigint,
+  `year` int
 );
